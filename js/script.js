725c31d7416a5f98aa5da1814c9f6e4150ab4150
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-03-26 16:44:39 +0100

    add: custom styles

--- js/script.js
+++ js/script.js
@@ -1,38 +1,28 @@
 
 //INPUT
-let userAge = prompt("Ciao utente! Quanti anni hai?"); // string | null
-console.log(userAge, typeof userAge);
-
-userAge = parseInt(userAge); // number
-console.log(userAge, typeof userAge);
-
-let userKm = prompt("Quanti Km vuoi percorrere?"); //string | null
-console.log (userKm, typeof userKm);
-
-userKm = parseInt(userKm); // number
-console.log(userKm, typeof userKm);
-
-
-//LOGICA
+// DATI
 const variableKm = 0.21; // number
 const youngDiscount = 20; // number
 const oldDiscount = 40; // number
 let discount ="";
 
-// calcolo del prezzo del biglietto in base all'età e al numero di Km che vuole fare il cliente
-if (userAge < 18 && userAge > 0) {
-    discount = (variableKm * userKm) * (youngDiscount / 100); 
-} else if (userAge > 65) {
-    discount = (variableKm * userKm) * (oldDiscount / 100); 
-} else {
-    discount = (variableKm * userKm);
-}
-console.log(discount, typeof discount);
+const btn = document.getElementById("form-btn");
+console.log(btn);
+
+btn.addEventListener("click", function () {
+    if (userAge < 18 && userAge > 0) {
+        discount = (variableKm * userKm) * (youngDiscount / 100); 
+    } else if (userAge > 65) {
+        discount = (variableKm * userKm) * (oldDiscount / 100); 
+    } else {
+        discount = (variableKm * userKm);
+    }
+});
+
 
 const finalPrice = (variableKm * userKm) - discount;
 
-// OUTPUT 
-//trasformare il numero con soli due valori dopo la virgola
+
 const number = finalPrice.toFixed(2);
 
 finalMessage = `Abbiamo calcolato il prezzo del tuo biglietto ed è di: ${number} €`; // string
